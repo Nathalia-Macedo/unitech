19a7a4dfe9ba452ba180d8bb98e0480700c00d43
Nathalia-Macedo <nathaliademacedomartins04@gmail.com>
2025-01-27 20:55:31 -0300

    subindo correcao no botao

--- src/Components/ComoFazemosDiferença.jsx
+++ src/Components/ComoFazemosDiferença.jsx
@@ -25,14 +25,14 @@ const ComoFazemosDiferenca = () => {
               ))}
             </ul>
 
-            <button className="bg-white text-[#1B4B96] font-semibold py-3 px-6 rounded-full hover:bg-opacity-90 transition duration-300 shadow-lg">
+           
             <Link
   to="/solicitar-demonstracao"
   className="inline-block w-full bg-white text-[#1B4B96] font-semibold py-3 px-6 rounded-full hover:bg-opacity-90 transition duration-300 shadow-lg text-center"
 >
   Solicite uma demonstração
 </Link>
-            </button>
+           
           </div>
 
           <div className="w-full lg:w-1/2">
